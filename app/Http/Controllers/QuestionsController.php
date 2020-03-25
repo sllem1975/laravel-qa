5e9788e75e298616fa7b6239d79fb379d6408667
sllem1975 <sllem1802@gmail.com>
2020-03-25 19:22:58 -0400

    Extend the form functionality to be able to update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -75,7 +75,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view("questions.edit", compact('question'));
     }
 
     /**
@@ -85,9 +85,11 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+        
+        return redirect('/questions')->with('success', "Your question has been updated");
     }
 
     /**
--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
+            <div class="card ">
+                <div class="card-header ">
                     <div class="d-flex align-itens-center">
-                        <h2>Ask Question</h2>
+                        <h2>Edit Question</h2>
                         <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-primary">Back to all
                                 Questions</a>
@@ -18,9 +18,9 @@
                 </div>
 
                 <div class="card-body">
-                    <form action=" {{ route('questions.store') }} " method="post">
-                        
-                        @include ("questions._form", ['buttonText' => "Ask Question"])
+                    <form action=" {{ route('questions.update', $question->id) }} " method="post">
+                        {{ method_field('PUT') }}
+                        @include ("questions._form", ['buttonText' => "Update Question"])
                     </form>
 
                 </div>
