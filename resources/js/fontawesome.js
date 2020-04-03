0319cf484a478ea36c6e1d5168590dab7d7adf10
sllem1975 <sllem1802@gmail.com>
2020-04-03 01:07:42 -0400

    Add vote controls to question and answer item

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,8 @@
+// Font Awesome
+import fontawesome from '@fortawesome/fontawesome-free'
+import faCareUp from '@fortawesome/fontawesome-free-solid/faCareUp';
+import faCareDown from '@fortawesome/fontawesome-free-solid/faCareDown';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+
+fontawesome.library.add([faCareup, faCareDown, faCheck, faStar]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    {{-- fonts awesome --}}
+    {{-- <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
